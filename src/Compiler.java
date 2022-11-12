e64edb63514081216c9d02bc7b77cf686cde0ae9
Celve <celve03@gmail.com>
2022-11-12 19:40:47 +0800

    feat(codegen): cater to OJ

--- src/Compiler.java
+++ src/Compiler.java
@@ -9,11 +9,11 @@ import org.antlr.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 public class Compiler {
   public static void main(String[] args) throws Exception {
+    System.setOut(new PrintStream(new BufferedOutputStream(new FileOutputStream("output.s")), true));
     MainKt.main(args);
   }
 }
